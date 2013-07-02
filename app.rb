b945f3700c3cfb57999efce5761122d3f1ec2ed5
Ruthie Nachmany <ruthienachmany@gmail.com>
2013-07-02 15:44:15 -0400

    added routes

--- app.rb
+++ app.rb
@@ -11,21 +11,65 @@ module Bikes
     # a complete set of routes for the station resource.
     # /stations/, /stations/1, /stations/new, /stations/edit/1, /stations/delete/1
 
-    get '/' do
-      "hello world"
-    end
+# list all stations
+get '/stations' do
+  @stations = Station.all
+  erb :index
+end
 
-    get '/stations' do
-      # list stations
-    end
+# add new station
+get '/stations/new' do
+  @station = Station.new
+  erb :new
+end
 
-    post '/stations' do
-      # create new station
-    end
+#show a single station
+get '/stations/:id' do
+  @station = Station.get(params[:id])
+  erb :show
+end
 
-    get '/stations/new' do
-      # create new station
-    end
+# create new station  
+post '/stations' do
+  @station = Station.new(params[:station])
+  if @station.save
+    status 201
+    redirect '/stations/' + @station.id.to_s
+  else
+    status 400
+    erb :new
+  end
+end
+
+# edit station
+get '/stations/edit/:id' do
+  @station = Station.get(params[:id])
+  erb :edit
+end
+
+# update station
+put '/stations/:id' do
+  @station = Station.get(params[:id])
+  if @station.update(params[:station])
+    status 201
+    redirect '/stations/' + params[:id]
+  else
+    status 400
+    erb :edit  
+  end
+end
+
+# delete station confirmation
+get '/stations/delete/:id' do
+  @station = Station.get(params[:id])
+  erb :delete
+end
+
+# delete station
+delete '/stations/:id' do
+  Station.get(params[:id]).destroy
+  redirect '/stations'  
+end
 
 
   end
