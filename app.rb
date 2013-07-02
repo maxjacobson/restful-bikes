804830704ea2fbcc818a8299ac4ea757fc6eb419
Max Jacobson <maxwell.jacobson@gmail.com>
2013-07-02 15:56:08 -0400

    added views

--- app.rb
+++ app.rb
@@ -7,69 +7,69 @@ DataMapper.auto_migrate!
 module Bikes
   class App < Sinatra::Base
 
-    # Now, following the example laid out in the reading create
-    # a complete set of routes for the station resource.
-    # /stations/, /stations/1, /stations/new, /stations/edit/1, /stations/delete/1
+    get '/' do
+      redirect "/stations"
+    end
 
-# list all stations
-get '/stations' do
-  @stations = Station.all
-  erb :index
-end
+    # list all stations
+    get '/stations' do
+      @stations = Station.all
+      erb :index
+    end
 
-# add new station
-get '/stations/new' do
-  @station = Station.new
-  erb :new
-end
+    # add new station
+    get '/stations/new' do
+      @station = Station.new
+      erb :new
+    end
 
-#show a single station
-get '/stations/:id' do
-  @station = Station.get(params[:id])
-  erb :show
-end
+    #show a single station
+    get '/stations/:id' do
+      @station = Station.get(params[:id])
+      erb :show
+    end
 
-# create new station  
-post '/stations' do
-  @station = Station.new(params[:station])
-  if @station.save
-    status 201
-    redirect '/stations/' + @station.id.to_s
-  else
-    status 400
-    erb :new
-  end
-end
+    # create new station  
+    post '/stations' do
+      @station = Station.new(params[:station])
+      if @station.save
+        status 201
+        redirect '/stations/' + @station.id.to_s
+      else
+        status 400
+        erb :new
+      end
+    end
 
-# edit station
-get '/stations/edit/:id' do
-  @station = Station.get(params[:id])
-  erb :edit
-end
+    # edit station
+    get '/stations/edit/:id' do
+      @station = Station.get(params[:id])
+      erb :edit
+    end
 
-# update station
-put '/stations/:id' do
-  @station = Station.get(params[:id])
-  if @station.update(params[:station])
-    status 201
-    redirect '/stations/' + params[:id]
-  else
-    status 400
-    erb :edit  
-  end
-end
+    # update station
+    put '/stations/:id' do
+      @station = Station.get(params[:id])
+      if @station.update(params[:station])
+        status 201
+        redirect '/stations/' + params[:id]
+      else
+        status 400
+        erb :edit  
+      end
+    end
 
-# delete station confirmation
-get '/stations/delete/:id' do
-  @station = Station.get(params[:id])
-  erb :delete
-end
+    # delete station confirmation
+    get '/stations/delete/:id' do
+      @station = Station.get(params[:id])
+      erb :delete
+    end
 
-# delete station
-delete '/stations/:id' do
-  Station.get(params[:id]).destroy
-  redirect '/stations'  
-end
+    # delete station
+    delete '/stations/:id' do
+      Station.get(params[:id]).destroy
+      redirect '/stations'  
+    end
 
 
   end
